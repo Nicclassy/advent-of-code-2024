9921c4be39044ff5d98298494906078f5da89cc0
Nicclassy <lucanapoli122@gmail.com>
2024-12-03 16:55:55 +1100

    Parse rows and add template

---
+++ lib/problem_template.ml
@@ -0,0 +1,10 @@
+let year = 2023
+let day = 1
+
+module Part_1 = struct
+  let run (input : string) : (string, string) result = Ok input
+end
+
+module Part_2 = struct
+  let run (input : string) : (string, string) result = Ok input
+end
--- lib/problems/utils.ml
+++ lib/problems/utils.ml
@@ -1,5 +1,5 @@
 let unzip_pairs lst = 
-  List.fold_left (fun (first, second) (x, y) -> (x :: first, y :: second)) ([], []) lst;;
+  List.fold_left (fun (first, second) (x, y) -> (x :: first, y :: second)) ([], []) lst
 
 let parse_list_columns input conversion =
   let lines = String.split_on_char '\n' input in
@@ -7,10 +7,18 @@ let parse_list_columns input conversion =
     match Str.split (Str.regexp " +") line with
     | [first_value; second_value] -> Some (conversion first_value, conversion second_value)
     | _ -> None
-  ) lines;;
+  ) lines
 
 let separate_and_sort_lists lst = 
   let (lst1, lst2) = unzip_pairs lst in
   let lst1_sorted = List.sort compare lst1 in
   let lst2_sorted = List.sort compare lst2 in
-  (lst1_sorted, lst2_sorted);;
+  (lst1_sorted, lst2_sorted)
+
+let parse_row conversion row =
+  let split_row = Str.split (Str.regexp " +") row in
+  List.map conversion split_row
+  
+let parse_rows input conversion = 
+  let rows = String.split_on_char '\n' input in
+  List.map (parse_row conversion) rows
